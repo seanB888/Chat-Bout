1426522ce934b1a4b9646b7c72ebddc9380e30d9
Sean Blake <sean.blake97@outlook.com>
2022-01-09 00:55:10 -0500

    Initial Commit

---
+++ Chat Bout/Chat_BoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_BoutApp.swift
+//  Chat Bout
+//
+//  Created by SEAN BLAKE on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Chat_BoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat Bout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chat Bout
+//
+//  Created by SEAN BLAKE on 1/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
